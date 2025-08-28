449dfb23618fd920b23b54fbe1051c4f8b393c45
Maël <m01@naudet.ch>
2025-08-28 10:23:57 +0200

    feat(ex marché) : étape 1 du marché complété
    fait en .net 4.7.2 (à modifier lol)

---
+++ Maël/Exercices/marché/PlaceDuMarche/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceDuMarche
+{
+    internal class Product
+    {
+        private int _place;
+        private string _sellerName;
+        private string _productName;
+        private int _quantity;
+        private string _unit;
+        private int _price;
+
+        public int Place { get { return _place; } set { _place = value; } }
+        public string SellerName { get { return _sellerName; } set { _sellerName = value; } }
+        public string ProductName { get { return _productName; } set { _productName = value; } }
+        public int Quantity { get { return _quantity; } set { _quantity = value; } }
+        public string Unit { get { return _unit; } set { _unit = value; } }
+        public int Price { get { return _price; } set { _price = value; } }
+
+        public Product(int place, string sellerName, string productName, int quantity, string unit, int price)
+        {
+            _place = place;
+            _sellerName = sellerName;
+            _productName = productName;
+            _quantity = quantity;
+            _unit = unit;
+            _price = price;
+        }
+    }
+}
---
+++ Maël/Exercices/marché/PlaceDuMarche/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace PlaceDuMarche
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            string strFileName = @"C:\Users\ps46bhd\Desktop\I323_Programmation-fonctionnelle\Maël\Exercices\marché\PlaceDuMarche\Resources\placeDuMarche.xlsx";
+            object missing = System.Reflection.Missing.Value;
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook workBook = excel.Application.Workbooks.Open(strFileName, missing, true, missing, missing, missing,
+            missing, missing, missing, true, missing, missing, missing, missing, missing);
+            Worksheet worksheet = (Worksheet)workBook.Sheets[2];
+
+
+            Range usedRange = worksheet.UsedRange;
+
+            for (int i = 2; i <= usedRange.Rows.Count; i++)
+            {
+                int place = Convert.ToInt16(worksheet.Cells[i, 1].Value);
+                string sellerName = worksheet.Cells[i, 2].Value;
+                string productName = worksheet.Cells[i, 3].Value;
+                int quantity = Convert.ToInt16(worksheet.Cells[i, 4].Value);
+                string unit = worksheet.Cells[i, 5].Value;
+                int price = Convert.ToInt16(worksheet.Cells[i, 6].Value);
+
+                Product product = new Product(place, sellerName, productName, quantity, unit, price);
+
+                products.Add(product);
+
+            }
+
+            numberOfSellers("pêches", products);
+            mostProducts("pastèques", products);
+
+            Console.ReadLine();
+
+            void numberOfSellers(string productName, List<Product> productList)
+            {
+                int number = 0;
+
+                foreach(Product product in products)
+                {
+                    if (product.ProductName.ToLower() == productName)
+                    {
+                        number++;
+                    }
+                }
+
+                Console.WriteLine($"Il y a {number} vendeurs de {productName}");
+            }
+
+            void mostProducts(string productName, List<Product> productList)
+            {
+                Product mostProduct = null;
+
+                foreach(Product product in products)
+                {
+                    if (mostProduct is null && product.ProductName.ToLower() == productName)
+                    {
+                        mostProduct = product;
+                    }
+                    else if (product.ProductName.ToLower() == productName && product.Quantity > mostProduct.Quantity)
+                    {
+                        mostProduct = product;
+                    }
+                }
+                if (mostProduct is null)
+                {
+                    Console.WriteLine($"Personne ne vend de {productName}");
+                } 
+                else
+                {
+                    Console.WriteLine($"C'est {mostProduct.SellerName} qui a le plus de {productName} (stand {mostProduct.Place}, {mostProduct.Quantity} {mostProduct.Unit}s)");
+                }
+            }
+        }
+    }
+}
